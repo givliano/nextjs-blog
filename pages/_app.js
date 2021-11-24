cab51d1d0253dcfc8647a2dacab40d72edb6c818
Giuliano Machado <giufma@gmail.com>
2021-11-24 20:31:59 -0300

    add _app for global styling

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import styles from '../styles/global.css'
+
+{/* Page for global styling */}
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default App;
